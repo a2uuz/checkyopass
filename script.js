aa3f9fe69bd5333a6894e35b7eca5c24012c6bf9
a2uuz <aziznageeye9@gmail.com>
2021-12-27 20:34:45 +0300

    checking the pass

---
+++ script.js
@@ -0,0 +1,10 @@
+/*This is a program that checks if your password is right 
+according to the input you give to it. by popping app a dialog box and asking
+your password, if you got it right than you will be shown an alert message  saying it's correct 
+if not it will keep asking you to enter the correct one.
+*/
+let pass;
+while(pass !== "azuz"){
+pass = prompt(`what is your passowrd sir`);
+}
+alert(`You answered correctly!!`);
